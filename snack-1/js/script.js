bb029dfc844671f730d541b61a65face034d249d
matteocenci <matteocenci01@gmail.com>
2024-04-15 15:56:33 +0200

    feat: array bikes

---
+++ snack-1/js/script.js
@@ -0,0 +1,23 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+
+const bikeList = [
+    {
+        bike: "Pantani's bike",
+        weigth: "25 kg",
+    },
+    {
+        bike: "TREK",
+        weigth: "16 kg",
+    },
+    {
+        bike: "Pittarosso",
+        weigth: "21 kg",
+    },
+    {
+        bike: "CUBE",
+        weigth:"32 kg",
+    },
+];
